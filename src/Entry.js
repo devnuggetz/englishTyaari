d1a5b5cc9bb75add7ffeda1867e5f88678095497
shuvamk <kshuvam564@gmail.com>
2020-10-22 03:41:55 -0700

    Selected a color pallette and finalized the design

--- src/Entry.js
+++ src/Entry.js
@@ -4,7 +4,7 @@ import './Entry.css'
 function Entry() {
     return (
         <div className='entry'>
-            <input type='text' placeholder='type the word'></input>
+            <input type='text' placeholder='Type the word'></input>
             <button>Submit</button>
         </div>
     )
--- src/Word.js
+++ src/Word.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Word.css'
+import HearingIcon from '@material-ui/icons/Hearing';
 
 function Word() {
     return (
         <div className='word'>
-            <h1>mellifelous</h1>
+            <div className='word__text'>
+                <HearingIcon />
+            </div>
+            
+            <p>1: having a smooth rich flow
+                a mellifluous voice
+                <br/>
+                2: filled with something (such as honey) that sweetens
+                mellifluous confections</p>
         </div>
     )
 }
