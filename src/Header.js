760555bb5e7d5e35c9be690453c5820c2fe0d6cc
shuvamk <kshuvam564@gmail.com>
2020-10-23 16:07:07 -0700

    Tried React-Reveal

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Header.css'
+import Pulse from 'react-reveal/Pulse';
 
 function Header() {
     return (
         <div className='header'>
+            <Pulse>
             <h1>English Tyaari</h1>
+            </Pulse>
         </div>
     )
 }
