ff4acb7fee99b9718077f324aed3a0a24ee447fe
shuvamk <kshuvam564@gmail.com>
2020-10-23 17:53:18 -0700

    Created skeleton for login page

--- src/Login.js
+++ src/Login.js
@@ -5,7 +5,16 @@ function Login() {
     return (
         <div className='login'>
             <div className='login__card'>
-
+                <div className='login__card--login'>
+                <h1>Already a member?</h1>
+                <input placeholder='Username'></input>
+                <input placeholder='Password'></input>
+                <button>Login</button>
+                </div>
+                <div className='login__card--google'>
+                <button>Login with Google</button>
+                </div>
+               
             </div>
         </div>
     )
